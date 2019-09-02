ca5d602beef473a05f6a35cd874142c2d08fe50e
Natallia Yeusiuchenia <ny@centaurea.io>
2019-09-02 14:41:55 +0300

    added es5 support

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,10 @@ module.exports = {
 	devtool: 'source-map',
 	performance: { hints: false },
 
-	entry: path.resolve(__dirname, 'src', 'ckeditor.js'),
-
+	entry: [
+		require.resolve('regenerator-runtime/runtime.js'),
+		path.resolve(__dirname, 'src', 'ckeditor.js')
+	],
 	output: {
 		// The name under which the editor will be exported.
 		library: 'ClassicEditor',
@@ -58,6 +60,17 @@ module.exports = {
 
 	module: {
 		rules: [
+			{
+            test: /ckeditor5-[^\/\\]+[\/\\].*\.js$/,
+            use: [
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ require( '@babel/preset-env' ) ]
+                    }
+                }
+            ]
+        },
 			{
 				test: /\.svg$/,
 				use: ['raw-loader']
