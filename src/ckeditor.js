a5ae5768c1878ae646591c983b6725eda589682e
Natallia Yeusiuchenia <ny@centaurea.io>
2019-09-06 18:00:36 +0300

    ckeditor updated

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -8,7 +8,6 @@ import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classicedi
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import List from '@ckeditor/ckeditor5-list/src/list';
@@ -27,7 +26,6 @@ ClassicEditor.builtinPlugins = [
 	Essentials,
 	Heading,
 	Bold,
-	Italic,
 	List,
 	BlockQuote,
 	Paragraph
@@ -40,7 +38,6 @@ ClassicEditor.defaultConfig = {
 			'heading',
 			'|',
 			'bold',
-			'italic',
 			'bulletedList',
 			'numberedList',
 			'blockQuote',
@@ -52,10 +49,9 @@ ClassicEditor.defaultConfig = {
 	heading: {
 		options: [
 			{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-			{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-			{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
 			{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
-			{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' }
+			{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+			{ model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' }
 		]
 	},
 
